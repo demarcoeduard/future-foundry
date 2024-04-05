7b9c8ecf0770d5027ad907f8e2925763ed38317a
demarcoeduard <edydumy@gmail.com>
2024-04-06 00:59:42 +0300

    added services and testimonials

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HomeComponent {
   @ViewChild('about') about!: ElementRef;
+  firstTestimonial = true;
+  secondTestimonial = false;
+  thirdTestimonial = false;
 
   onScroll() {
     this.about.nativeElement.scrollIntoView({behavior: 'smooth'});
   }
 
+  onShowTestimonial(num: number) {
+    this.firstTestimonial = num === 1;
+    this.secondTestimonial = num === 2;
+    this.thirdTestimonial = num === 3;
+  }
 }
